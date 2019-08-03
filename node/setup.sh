b71915705f90a534a6554194074a20b236af3c2f
Clairton Luz <clairton.c.l@gmail.com>
2019-08-03 18:13:27 -0300

    configurando ambiente node

---
+++ node/setup.sh
@@ -0,0 +1,10 @@
+yarn add -D typescript sucrase nodemon
+yarn add -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+yarn add -D prettier eslint-config-prettier eslint-plugin-prettier
+yarn eslint --init
+
+# eslint
+# adicionar "parser", "plugins" e "extends" que está no ".eslintrc.js" para o
+# seu gerado pelo o comando "yarn eslint --init"
+
+
